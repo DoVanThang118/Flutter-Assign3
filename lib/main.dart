fc9467e06cd33e5cb92642e6973419300911e18d
WinHo <dovanthang118@gmail.com>
2024-01-22 16:15:34 +0700

    new

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'student.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final ListStudent studentList = ListStudent();
+
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StudentFormScreen(studentList: studentList),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
